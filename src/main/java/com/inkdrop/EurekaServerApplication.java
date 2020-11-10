d287112b414267b8bb9a73b487a882b0869c038b
skritik098 <sachdevakritik440@gmail.com>
2020-11-10 21:06:33 +0530

    first

---
+++ src/main/java/com/inkdrop/EurekaServerApplication.java
@@ -0,0 +1,20 @@
+package com.inkdrop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+
+//import brave.context.log4j2.ThreadContextScopeDecorator;
+//import brave.propagation.CurrentTraceContextScopeDecorator;
+
+@EnableAutoConfiguration
+@EnableEurekaServer
+@PropertySource("classpath:properties/db.properties")
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
